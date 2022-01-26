edc2f97eec29d824069bc494ac7d66ca04b7056d
kimhwijin <rlagnlwls0160@gmail.com>
2022-01-26 13:48:52 +0900

    add param make inceptionV4, add modules Inception, ResNetUnit, SEBlock, Xception

--- kfood_model.py
+++ kfood_model.py
@@ -68,7 +68,7 @@ class Stem(keras.layers.Layer):
         #conv 96 3x3 s + conv 96 3x3 s
         #conv 192 3x3 v / max pool 2 v
         
-                                               #299x299x3
+        #299x299x3
         self.conv1 = conv2d_bn(32, '3x3', 'v', 2) #149x149x3
         self.conv2 = conv2d_bn(32, '3x3', 'v', 1) #147x147x3
         self.conv3 = conv2d_bn(64, '3x3', 's', 1) #147x147x3
@@ -299,9 +299,8 @@ class InceptionC(keras.layers.Layer):
 
         return self.concat([Z_1, Z_2, Z_3_1, Z_3_2, Z_4_1, Z_4_2])
 
-
-def make_inception():
-    inputs = keras.layers.Input(shape=[299, 299, 3])
+def make_inceptionV4(input_shape=[299, 299, 3], output_dim=150):
+    inputs = keras.layers.Input(shape=input_shape)
     stem_module = Stem()
     Z = stem_module(inputs)
 
@@ -317,7 +316,7 @@ def make_inception():
 
     #7 x inceptioin B
     inceptionB_modules = []
-    for _ in range(4):
+    for _ in range(7):
         inceptionB_modules.append(InceptionB())
     for inceptionB_module in inceptionB_modules:
         Z = inceptionB_module(Z)
@@ -334,7 +333,98 @@ def make_inception():
 
     Z = keras.layers.GlobalAveragePooling2D()(Z)
     Z = keras.layers.Dropout(0.8)(Z)
-    outputs = keras.layers.Dense(150, activation='softmax')(Z)
+    outputs = keras.layers.Dense(output_dim, activation='softmax')(Z)
 
     model = keras.models.Model(inputs=inputs, outputs=outputs)
+
     return model
+
+
+
+
+class InceptionModule(keras.layers.Layer):
+    def __init__(self, filters, **kwargs):
+        super().__init__(**kwargs)
+        filters_1x1 = filters[0:2] + filters[3:4] + filters[5:]
+        filters_3x3 = filters[2]
+        filters_5x5 = filters[4]
+        self.conv1x1_1 = keras.layers.Conv2D(filters_1x1[0], kernel_size=1, strides=1, padding="same")
+        self.conv1x1_2 = keras.layers.Conv2D(filters_1x1[1], kernel_size=1, strides=1, padding="same")
+        self.conv1x1_3 = keras.layers.Conv2D(filters_1x1[2], kernel_size=1, strides=1, padding="same")
+        self.conv1x1_4 = keras.layers.Conv2D(filters_1x1[3], kernel_size=1, strides=1, padding="same")
+
+        self.conv3 = keras.layers.Conv2D(filters_3x3, kernel_size=3, strides=1, padding="same")
+        self.conv5 = keras.layers.Conv2D(filters_5x5, kernel_size=3, strides=1, padding="same")
+
+        self.max_pool = keras.layers.MaxPooling2D(pool_size=3, strides=1, padding="same")
+
+    def call(self, inputs):
+        Z_1 = self.conv1x1_1(inputs)
+
+        Z_2 = self.conv1x1_2(inputs)
+        Z_2 = self.conv3(Z_2)
+
+        Z_3 = self.conv1x1_3(inputs)
+        Z_3 = self.conv5(Z_3)
+
+        Z_4 = self.max_pool(inputs)
+        Z_4 = self.conv1x1_4(Z_4)
+
+        return keras.layers.Concatenate(axis=-1)([Z_1, Z_2, Z_3, Z_4])
+
+class ResNetUnit(keras.layers.Layer):
+    def __init__(self, filters, strides=1, activation='relu', **kwargs):
+        super().__init__(**kwargs)
+        self.activation = keras.activations.get(activation)
+        self.main_layers = [
+            keras.layers.Conv2D(filters, 3, strides=strides, padding="samen", use_bias=False),
+            keras.layers.BatchNormalization(),
+            self.activation,
+            keras.layers.Conv2D(filters, 3, strides=1, padding="same", use_bias=False),
+            keras.layers.BatchNormalization()
+        ]
+        self.skip_layers = []
+        if strides > 1:
+            self.skip_layers = [
+                keras.layers.Conv2D(filters, kernel_size=1, strides=strides, padding="same", use_bias=False),
+                keras.layers.BatchNormalization()
+            ]
+
+
+    def call(self, inputs):
+        Z = inputs
+        for layer in self.main_layers:
+            Z = layer(Z)
+        skip_Z = inputs
+        for layer in self.skip_layers:
+            skip_Z = layer(skip_Z)
+        
+        return self.activation(skip_Z + Z)
+
+
+class SE_Block(keras.layers.Layer):
+    def __init__(self, filters, ratio=16, **kwargs):
+        super().__init__(**kwargs)
+        self.global_avg_pool = keras.layers.GlobalAveragePooling2D()
+        self.squeeze = keras.layers.Dense(filters//ratio, activation='relu')
+        self.excitation = keras.layers.Dense(filters, activation='sigmoid')
+
+    def call(self, inputs):
+        Z = self.global_avg_pool(inputs)
+        Z = self.squeeze(Z)
+        return self.excitation(Z)
+
+
+class XceptionModule(keras.layers.Layer):
+    def __init__(self, filter, **kwrags):
+        super().__init__(**kwargs)
+        self.depthwise = keras.layers.Conv2D(filter, kernel_size=3, strides=1, padding='same')
+        self.pointwise = keras.layers.Conv2D(filter, kernel_size=1, strides=1, padding="same")
+    
+    def call(self, inputs):
+        Z = inputs
+        Z = self.depthwise(Z)
+        Z = keras.layers.BatchNormalization()(Z)
+        Z = self.pointwise(Z)
+        return keras.layers.BatchNormalization()(Z)
+
