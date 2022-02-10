cf0fed0d06345e5cdb36471f0df1e98c72fb4a55
kimhwijin <rlagnlwls0160@gmail.com>
2022-02-10 15:57:33 +0900

    keras inception se block structure, train func

--- application/keras_inception_resnet_v2_se.py
+++ application/keras_inception_resnet_v2_se.py
@@ -3,7 +3,7 @@ from application.inception import *
 from application.squeeze_excitation_block import *
 from application.WEIGHTS import KerasInceptionResNetV2_Weights
 
-def InceptionResNetV2SEBlock(input_shape=[299, 299, 3], n_classes=150, squeeze_ratio=4):
+def KerasInceptionResNetV2SEBlock(input_shape=[299, 299, 3], n_classes=150, squeeze_ratio=4):
     img_input = keras.layers.Input(shape=input_shape)
 
     weights = KerasInceptionResNetV2_Weights()
@@ -12,25 +12,25 @@ def InceptionResNetV2SEBlock(input_shape=[299, 299, 3], n_classes=150, squeeze_r
 
     #35 x 35 x 320
     channel = keras.backend.int_shape(x)[-1]
-    for _ in range(7):
+    for _ in range(10):
         x = block35(x, weights.BLOCK35)
-        x = se_block(x, channel, squeeze_ratio)
-    
+
+    x = se_block(x, channel, squeeze_ratio)
     x = reduction_A(x, weights.REDUCTION_A)
 
     #17 x 17 x 1088
     channel = keras.backend.int_shape(x)[-1]
-    for _ in range(12):
+    for _ in range(20):
         x = block17(x, weights.BLOCK17)
-        x = se_block(x, channel, squeeze_ratio)
 
+    x = se_block(x, channel, squeeze_ratio)
     x = reduction_B(x, weights.REDUCTION_B)
 
     #8 x 8 x 2080
     channel = keras.backend.int_shape(x)[-1]
-    for _ in range(6):
+    for _ in range(9):
         x = block8(x, weights.BLOCK8)
-        x = se_block(x, channel, squeeze_ratio)
+    x = se_block(x, channel, squeeze_ratio)
     
     x = block8(x, filters=weights.BLOCK8, scale=1., activation=None)
     x = se_block(x, channel, squeeze_ratio)
